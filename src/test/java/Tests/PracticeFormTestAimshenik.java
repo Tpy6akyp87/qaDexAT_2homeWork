0e27fea9d2aad073596cf99303f5e4459eebcac6
Tpy6akyp87 <kaellakhim@gmail.com>
2022-01-17 08:35:19 +0200

    done

--- src/test/java/Tests/PracticeFormTestAimshenik.java
+++ src/test/java/Tests/PracticeFormTestAimshenik.java
@@ -1,5 +1,7 @@
 package Tests;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 
@@ -7,25 +9,19 @@ import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.by;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTestAimshenik {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize="1920x1080";
+    }
+
     @Test
     void successTest(){
         open("https://demoqa.com/automation-practice-form");
-        //lastName
-        //userEmail
-        //class="col-md-9 col-sm-12"
-        //userNumber
-        //dateOfBirthInput  react-datepicker__month-select  May   react-datepicker__year-select   react-datepicker__day react-datepicker__day--016 react-datepicker__day--selected react-datepicker__day--weekend
-        //subjects-auto-complete__control css-yk16xz-control
-        //col-md-9 col-sm-12    hobbies-checkbox-1
-        //uploadPicture
-        //Current Address
-        // css-1wa3eu0-placeholder  css-1uccc91-singleValue  Uttar Pradesh
-        // css-yk16xz-control    css-1uccc91-singleValue    Lucknow
 
         $("#firstName").setValue("Imshenik");
         $("#lastName").setValue("Anatoly");
@@ -38,27 +34,31 @@ public class PracticeFormTestAimshenik {
         $("[class=react-datepicker__year-select]").click();
         $(byText("1987")).click();
         $(byText("16")).click();
-        $("[class=subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3]").setValue("Maths").pressEnter();
-        $("[class=col-md-9 col-sm-12]").$("#hobbies-checkbox-1").click();
-        //$("#uploadPicture").click();
-        File file = $("#uploadPicture").uploadFile(new File("src/test/resources/vannila-dwarf.jpg"));
-        $("#currentAddress").scrollTo().setValue("на работе");
-        $("[class= css-1wy0on6]").$("#Uttar Pradesh").click();
-        $("[class= css-yk16xz-control]").$("#Lucknow").click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $(byText("Reading")).click();
+        File file =new File("src/test/resources/vannila-dwarf.jpg");
+        Selenide.$(byId("uploadPicture")).uploadFile(file);
+        $("#currentAddress").setValue("на работе");
+        $("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
+        $("#react-select-4-input").setValue("Lucknow").pressEnter();
 
         $("#submit").scrollTo().click();
+
         $("#example-modal-sizes-title-lg").shouldBe(visible);
 
-        $("[class=table table-dark table-striped table-bordered table-hover]").shouldHave(text("Anatoly"));
-
-
-
+        $("[class=table-responsive]").shouldHave(text("Imshenik"));
+        $("[class=table-responsive]").shouldHave(text("Anatoly"));
+        $("[class=table-responsive]").shouldHave(text("ichi111@yandex.ru"));
+        $("[class=table-responsive]").shouldHave(text("Male"));
+        $("[class=table-responsive]").shouldHave(text("9268885522"));
+        $("[class=table-responsive]").shouldHave(text("16 May,1987"));
+        $("[class=table-responsive]").shouldHave(text("Maths"));
+        $("[class=table-responsive]").shouldHave(text("Reading"));
+        $("[class=table-responsive]").shouldHave(text("на работе"));
+        $("[class=table-responsive]").shouldHave(text("Uttar Pradesh"));
+        $("[class=table-responsive]").shouldHave(text("Lucknow"));
+        $("[class=table-responsive]").shouldHave(text("vannila-dwarf.jpg"));
 
     }
-
-
-
-
-
 
 }
